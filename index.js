c82ee83dfdbc5597df8d15c9870401057b71fa04
Diex01 <thediex01@gmail.com>
2016-07-07 16:14:34 +0300

    add

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 var request = require('request');
+var obfusYa = require('obfusYa');
 
+var data = 'https://raw.githubusercontent.com/FrontendSimf20016/obfuscator/master/data.json';
+
+request({
+    url: data,
+    json: true
+}, function(err, res, body) {
+    if (!err && res.statusCode === 200) {
+        var resData = obfusYa(body);
+        console.log(body[0] + ':' + resData[body[0]]);
+    }
+});
